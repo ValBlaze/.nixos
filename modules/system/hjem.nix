9f93245470502427056effab8c01d96b5babafa7
Val Blaze <valblazeyt@gmail.com>
2025-12-05 02:51:35 -0600

    gen

--- modules/system/hjem.nix
+++ modules/system/hjem.nix
@@ -1,12 +1,8 @@
 { inputs, config, pkgs, ... }: {
-  imports = [
-  ];
 
   hjem.users.valblaze = {
     user = "valblaze";
     directory = "/home/valblaze";
   };
-  
-  hjem.linker = inputs.hjem.packages.${pkgs.stdenv.hostPlatform.system}.smfh;
 }
 
