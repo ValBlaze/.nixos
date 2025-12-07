14f33fd10b390b91268c1649e2881c8478542068
Val Blaze <valblazeyt@gmail.com>
2025-12-06 23:34:45 -0600

    221   2025-12-06 23:25:59   (current)

--- modules/scripts/nrs/nrs.sh
+++ modules/scripts/nrs/nrs.sh
@@ -2,7 +2,6 @@
 set -e
 
 pushd ~/.nixos
-echo "Running as: $(whoami)"
 gen="$(sudo nix-env --list-generations --profile /nix/var/nix/profiles/system | grep current)"
 git commit -am "$gen"
 git push
