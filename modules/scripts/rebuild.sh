c5cc75af316c10793d89f08d208dbdd01d7353ac
Val Blaze <valblazeyt@gmail.com>
2025-12-07 00:53:40 -0600

    gen

--- modules/scripts/rebuild.sh
+++ modules/scripts/rebuild.sh
@@ -2,8 +2,7 @@
 set -x
 set -e
 pushd ~/.nixos
-current_gen=$(sudo nix-env --list-generations --profile /nix/var/nix/profiles/system | grep current | awk '{print $1}')
-new_gen=$((current_gen + 1))
+gen=$(sudo nix-env --list-generations --profile /nix/var/nix/profiles/system | grep current | awk '{print $1}')
 git add .
 git commit -m "gen $new_gen"
 sudo nixos-rebuild switch --flake .
