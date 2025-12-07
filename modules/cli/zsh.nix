91c5a4b1dfb5151c1dd4903581ad078df752436c
Val Blaze <valblazeyt@gmail.com>
2025-12-07 13:32:22 -0600

    gen 233

--- modules/cli/zsh.nix
+++ modules/cli/zsh.nix
@@ -5,11 +5,12 @@
     zsh-syntax-highlighting
     nix-zsh-completions
     nix-search-tv
+    fzf
   ];
 
   environment.shellAliases = {
     nrs = "/home/valblaze/.nixos/modules/scripts/rebuild.sh";
-    ns="nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
+    ns = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
   };
 
   programs.zsh = {
