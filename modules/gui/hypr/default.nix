9bb928fb5b04d92927d2bf3327a505ff4096b0ca
Val Blaze <valblazeyt@gmail.com>
2025-12-07 01:39:07 -0600

    gen 225

--- modules/gui/hypr/default.nix
+++ modules/gui/hypr/default.nix
@@ -3,7 +3,7 @@
 {
   hjem.users.valblaze = {
     files.".config/hypr".source = "/home/valblaze/.nixos/modules/gui/hypr";
-    programs.hyprland.enable = true;
+    rum.programs.hyprland.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
