a6e6be3927ec3eb1975d5a8f2f3d9326eea9437a
Val Blaze <valblazeyt@gmail.com>
2025-12-11 00:41:29 -0600

    gen 249

--- modules/cli/nvim/default.nix
+++ modules/cli/nvim/default.nix
@@ -2,21 +2,28 @@
 
 {
   neovim = pkgs.neovim-unwrapped;
-  extraBinPath = [];
-
   initLua = ''
     require('config')
-    require('lz.n').load('lazy')
+    LZN = require('lz.n')
+    LZN.load('lazy')
   '';
+
+  extraBinPath = with pkgs; [
+    lua-language-server
+    stylua
+    ripgrep # telescope dep
+    fd # telescope dep
+  ];
 
   plugins = {
     start = with pkgs.vimPlugins; [
       lz-n
-      plenary-nvim # dep of telescope
+      plenary-nvim # telescope dep
       oil-nvim
     ];
 
     opt = with pkgs.vimPlugins; [
+      nvim-treesitter.withAllGrammars.dependencies;
       telescope-nvim
       blink-cmp
     ];
