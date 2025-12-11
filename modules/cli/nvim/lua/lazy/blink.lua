91ebf7382942df9b5fac4b1a783bfe475ba57d03
Val Blaze <valblazeyt@gmail.com>
2025-12-11 00:55:31 -0600

    gen 250

--- modules/cli/nvim/lua/lazy/blink.lua
+++ modules/cli/nvim/lua/lazy/blink.lua
@@ -1,5 +1,5 @@
 return {
-    { 
+    {
       "blink.cmp",
       event = "DeferredUIEnter",
       after = function()
@@ -18,12 +18,23 @@ return {
           },
 
           sources = {
-            default = { "lsp", "path", "snippets" }
+            default = { "lsp", "path", "snippets", "buffer" }
           },
 
-          fuzzy = { implementation = "lua" },
+          fuzzy = { implementation = "prefer_rust_with_warning" },
 
           signature = { enabled = true },
+        })
+
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities({}, false))
+        capabilities = vim.tbl_deep_extend('force', capabilities, {
+          textDocument = {
+            foldingRange = {
+              dynamicRegistration = false,
+              lineFoldingOnly = true
+            }
+          }
         })
       end
     }
--- modules/cli/nvim/lua/lazy/lspconfig.lua
+++ modules/cli/nvim/lua/lazy/lspconfig.lua
@@ -2,5 +2,8 @@ return {
     {
         "nvim-lspconfig",
         event = "DeferredUIEnter",
+        before = function()
+          LZN.trigger_load("
+        end,
     },
 }
