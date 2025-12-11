08e11c25bc1329e7077f13e66bbce9e326c22034
Val Blaze <valblazeyt@gmail.com>
2025-12-10 23:39:26 -0600

    gen 246

--- modules/cli/nvim/default.nix
+++ modules/cli/nvim/default.nix
@@ -1,31 +1,28 @@
 { inputs, pkgs, ... }: {
 
-  programs.mnw = {
-    enable = true;
-    neovim = pkgs.neovim-unwrapped;
-    extraBinPath = [];
+  neovim = pkgs.neovim-unwrapped;
+  extraBinPath = [];
 
-    initLua = ''
-      require('config')
-      require('lz.n').load('lazy')
-    '';
+  initLua = ''
+    require('config')
+    require('lz.n').load('lazy')
+  '';
 
-    plugins = {
-      start = with pkgs.vimPlugins; [
-        lz-n
-        plenary-nvim # dep of telescope
-        oil-nvim
-      ];
+  plugins = {
+    start = with pkgs.vimPlugins; [
+      lz-n
+      plenary-nvim # dep of telescope
+      oil-nvim
+    ];
 
-      opt = with pkgs.vimPlugins; [
-        telescope-nvim
-      ];
+    opt = with pkgs.vimPlugins; [
+      telescope-nvim
+    ];
 
-      dev.config = {
-        pure = ./.;
-        impure = "/home/valblaze/.nixos/modules/cli/nvim";
-      };
+    dev.config = {
+      pure = ./.;
+      impure = "/home/valblaze/.nixos/modules/cli/nvim";
     };
   };
-}
+};
 
