37cfd6f460f50d1dc12368b863d984f0f8b95087
Val Blaze <valblazeyt@gmail.com>
2025-12-14 02:29:20 -0600

    gen 274

--- modules/cli/zsh.nix
+++ modules/cli/zsh.nix
@@ -3,22 +3,10 @@
   environment.systemPackages = with pkgs; [
     nix-search-tv
     fzf
+
+    pkgs.bat
+    pkgs.bat-extras.core
   ];
-
-  environment.shellAliases = {
-    nrs = "/home/valblaze/.nixos/modules/scripts/rebuild.sh";
-    ns = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
-  };
-
-  programs.zoxide = {
-    enable = true;
-    flags = [
-      "--cmd cd"
-    ];
-  };
-
-  programs.starship.enable = true;
-  programs.yazi.enable = true;
 
   programs.zsh = {
     enable = true;
@@ -32,6 +20,37 @@
         [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
         rm -f -- "$tmp"
       }
+
+      # To use batpipe, eval the output of this command in your shell init script.
+      LESSOPEN="|/nix/store/4ygcpcvlzwrxpykp4171zaf07rdj5q33-batpipe-2024.08.24-unstable-2025-02-22/bin/.batpipe-wrapped %s";
+      export LESSOPEN;
+      unset LESSCLOSE;
+
+      # The following will enable colors when using batpipe with less:
+      LESS="$LESS -R";
+      BATPIPE="color";
+      export LESS;
+      export BATPIPE;
     '';
   };
+
+  environment.shellAliases = {
+    nrs = "/home/valblaze/.nixos/modules/scripts/rebuild.sh";
+    ns = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
+    cat = "bat";
+    diff = "batdiff";
+    grep = "batgrep";
+    man = "batman";
+    watch = "batwatch";
+  };
+
+  programs.zoxide = {
+    enable = true;
+    flags = [
+      "--cmd cd"
+    ];
+  };
+
+  programs.starship.enable = true;
+  programs.yazi.enable = true;
 }
--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -9,14 +9,6 @@
     pkgs.gcc
     pkgs.dig
     pkgs.wofi
-    pkgs.bat
-    pkgs.bat-extras.batdiff
-    pkgs.bat-extras.batgrep
-    pkgs.bat-extras.batman
-    pkgs.bat-extras.batpipe
-    pkgs.bat-extras.batwatch
-    pkgs.bat-extras.core
-    pkgs.bat-extras.prettybat
     pkgs.gh
     pkgs.wl-clipboard
     self'.packages.neovimDev
