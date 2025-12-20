c6fd433bc06d9d2e712115adff2928ae9295b698
Val Blaze <valblazeyt@gmail.com>
2025-12-19 18:26:37 -0600

    gen 310

--- modules/cli/zsh.nix
+++ modules/cli/zsh.nix
@@ -1,4 +1,10 @@
-{ config, lib, pkgs, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
 
   environment.systemPackages = with pkgs; [
     nix-search-tv
@@ -31,6 +37,13 @@
       BATPIPE="color";
       export LESS;
       export BATPIPE;
+
+      export FZF_DEFAULT_OPTS=" \
+      --color=bg+:#363A4F,bg:#24273A,spinner:#F4DBD6,hl:#ED8796 \
+      --color=fg:#CAD3F5,header:#ED8796,info:#C6A0F6,pointer:#F4DBD6 \
+      --color=marker:#B7BDF8,fg+:#CAD3F5,prompt:#C6A0F6,hl+:#ED8796 \
+      --color=selected-bg:#494D64 \
+      --color=border:#6E738D,label:#CAD3F5"
     '';
   };
 
