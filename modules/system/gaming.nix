7d6a1d38d84819959968f0d8c3a0b3e004f7af15
valblaze <valblazeyt@gmail.com>
2025-12-22 23:54:54 -0600

    gen

---
+++ modules/system/gaming.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+
+{
+  programs.gamemode = {
+    enable = true;
+    enableRenice = true;
+  };
+  programs.gamescope.enable = true;
+
+  programs.steam = {
+    dedicatedServer.openFirewall = true;
+    remotePlay.openFirewall = true;
+    extest.enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+    gamescopeSession.enable = true;
+    protontricks.enable = true;
+  };
+}
