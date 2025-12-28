9051867aaca17f9bd620357cae43af29b3b1884c
Val Blaze <valblazeyt@gmail.com>
2025-12-28 06:54:19 -0600

    gen

--- modules/cli/nvim/lua/config/init.lua
+++ modules/cli/nvim/lua/config/init.lua
@@ -4,16 +4,9 @@
 vim.g.mapleader = " "
 
 -- [[ Setting options ]] See `:h vim.o`
--- NOTE: You can change these options as you wish!
--- For more options, you can see `:help option-list`
--- To see documentation for an option, you can use `:h 'optionname'`, for example `:h 'number'`
--- (Note the single quotes)
+-- NOTE: For more options, you can see `:help option-list`
 
--- Print the line number in front of each line
 vim.o.number = true
-
--- Use relative line numbers, so that it is easier to jump with j, k. This will affect the 'number'
--- option above, see `:h number_relativenumber`
 vim.o.relativenumber = true
 
 -- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can
@@ -25,28 +18,26 @@ vim.api.nvim_create_autocmd("UIEnter", {
   end,
 })
 
+vim.o.undofile = true
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
--- Highlight the line where the cursor is on
 vim.o.cursorline = true
 
--- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
 
--- Show <tab> and trailing spaces
 vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
 vim.o.confirm = true
 
 vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = false
+vim.o.shiftwidth = 0
 
--- [[ Set up keymaps ]] See `:h vim.keymap.set()`, `:h mapping`, `:h keycodes`
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
 
 -- Use <Esc> to exit terminal mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
@@ -61,11 +52,15 @@ vim.keymap.set({ "n" }, "<A-j>", "<C-w>j")
 vim.keymap.set({ "n" }, "<A-k>", "<C-w>k")
 vim.keymap.set({ "n" }, "<A-l>", "<C-w>l")
 
+-- Map <C-e>, <C-y> to scroll in insert mode but lose the ability to copy text from the line above/below the cursor
+vim.keymap.set("i", "<C-e>", "<C-x><C-e>")
+vim.keymap.set("i", "<C-y>", "<C-x><C-y>")
+
 -- [[ Basic Autocommands ]].
 -- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
 
 -- Highlight when yanking (copying) text.
--- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
+-- See `:h vim.hl.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   callback = function()
@@ -90,3 +85,4 @@ end, { desc = "Print the git blame for the current line" })
 -- For example, to add the "nohlsearch" package to automatically turn off search highlighting after
 -- 'updatetime' and when going to insert mode
 vim.cmd("packadd! nohlsearch")
+vim.cmd("packadd! nvim.undotree")
