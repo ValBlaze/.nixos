beb89fcd6ac231650ca0f0fcf307cc0cf4efed27
Val Blaze <valblazeyt@gmail.com>
2025-12-28 19:35:26 -0600

    gen

--- modules/cli/nvim/plugin/catppuccin.lua
+++ modules/cli/nvim/plugin/catppuccin.lua
@@ -1,6 +1,13 @@
 require("catppuccin").setup({
   flavour = "macchiato",
   transparent_background = true,
+  float = {
+    transparent = true,
+    solid = true,
+  },
+  integrations = {
+    fzf = true,
+  },
 })
 
 vim.cmd.colorscheme("catppuccin")
