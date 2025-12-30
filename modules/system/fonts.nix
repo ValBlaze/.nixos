b93da8c891358cebc3f4fe445e8516446358e240
Val Blaze <valblazeyt@gmail.com>
2025-12-29 19:39:14 -0600

    gen

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,8 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    inter
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      inter
+			merriweather
+    ];
+
+		fontconfig.defaultFonts = {
+			emoji = [ "Noto Color Emoji" ];
+			monospace = [ "JetBrainsMono Nerd Font Mono" ];
+			sansSerif = [ "Inter Variable" ];
+			serif = [ "Merriweather" ];
+		};
+  };
 }
