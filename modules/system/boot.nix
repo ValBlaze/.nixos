9aa5be6e3d3ca080c9a5922c5539c491cf9bd13f
valblaze <valblazeyt@gmail.com>
2026-06-12 03:37:28 -0500

    feat: limine secure boot and win11

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -4,8 +4,26 @@
 }:
 
 {
-  boot.loader.limine.enable = true;
-  boot.loader.limine.style.wallpapers = [ ../../assets/wallpapers/limine-splash.png ];
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    loader = {
+      limine = {
+        enable = true;
+        secureBoot.enable = true;
+        secureBoot.autoEnrollKeys.enable = true;
+        extraEntries = ''
+          /Windows
+            protocol: efi
+            path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
+        '';
+        extraConfig = ''
+          remember_last_entry: yes
+        '';
+        style.wallpapers = [ ../../assets/wallpapers/limine-splash.png ];
+      };
+
+      efi.canTouchEfiVariables = true;
+    };
+
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
 }
