16aa4a4429ba81ca0c489c62f278c831c327fcc6
valblaze <valblazeyt@gmail.com>
2026-06-13 22:38:59 -0500

    sync

--- modules/desktop/utils.nix
+++ modules/desktop/utils.nix
@@ -7,6 +7,8 @@
   environment.systemPackages = with pkgs; [
     libnotify
     wl-clipboard
+    xclip
+    wineWow64Packages.full
   ];
 
   programs.ydotool.enable = true;
--- modules/dev/editors.nix
+++ modules/dev/editors.nix
@@ -5,7 +5,6 @@
 
 {
   environment.systemPackages = with pkgs; [
-    helix
     zed-editor
     nil
     nixd
