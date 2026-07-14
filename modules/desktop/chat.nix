470e72f8f4b78513aeb3664bd072f7b9dc52f0fc
valblaze <valblazeyt@gmail.com>
2026-07-13 21:44:17 -0500

    sync

--- modules/desktop/chat.nix
+++ modules/desktop/chat.nix
@@ -5,7 +5,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-    discord
+    (discord.override {
+      withEquicord = true;
+    })
     element-desktop
   ];
 }
--- modules/shell/variables.nix
+++ modules/shell/variables.nix
@@ -4,11 +4,7 @@
 
 {
   environment.sessionVariables = {
-    QT_QPA_PLATFORM = "wayland;xcb";
-    QT_QPA_PLATFORMTHEME = "qt6ct";
-
     NIXOS_OZONE_WL = "1";
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
 
     EDITOR = "nvim";
     VISUAL = "nvim";
