f9849fa6ab038f3bf796a396b418d8062f13fc16
valblaze <valblazeyt@gmail.com>
2026-07-18 21:42:30 -0500

    sync

--- modules/system/systemd.nix
+++ modules/system/systemd.nix
@@ -6,25 +6,25 @@
 }:
 
 {
-  systemd.user.services.nixos-rebuild-watch = {
-    wantedBy = [ "default.target" ];
-    serviceConfig.Type = "exec";
-
-    path = with pkgs; [
-      watchexec
-      coreutils
-    ];
-
-    script = ''
-      watchexec -w /home/valblaze/.nixos -e nix --postpone --notify -- systemctl start nixos-rebuild
-    '';
-  };
-
-  systemd.services.nixos-rebuild = {
-    serviceConfig.Type = "oneshot";
-
-    script = ''
-      ${config.system.build.nixos-rebuild}/bin/nixos-rebuild switch --flake path:/home/valblaze/.nixos
-    '';
-  };
+  # systemd.user.services.nixos-rebuild-watch = {
+  #   wantedBy = [ "default.target" ];
+  #   serviceConfig.Type = "exec";
+  #
+  #   path = with pkgs; [
+  #     watchexec
+  #     coreutils
+  #   ];
+  #
+  #   script = ''
+  #     watchexec -w /home/valblaze/.nixos -e nix --postpone --notify -- systemctl start nixos-rebuild
+  #   '';
+  # };
+  #
+  # systemd.services.nixos-rebuild = {
+  #   serviceConfig.Type = "oneshot";
+  #
+  #   script = ''
+  #     ${config.system.build.nixos-rebuild}/bin/nixos-rebuild switch --flake path:/home/valblaze/.nixos
+  #   '';
+  # };
 }
