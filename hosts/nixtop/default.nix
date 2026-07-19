526ddf3c85aba8639e133e18844fabc6c0af4ccc
valblaze <valblazeyt@gmail.com>
2026-07-19 03:26:32 -0500

    sync

--- hosts/nixtop/default.nix
+++ hosts/nixtop/default.nix
@@ -8,12 +8,16 @@
 {
   flake.nixosConfigurations.nixtop = inputs.nixpkgs.lib.nixosSystem {
     modules = [
-      ./hardware-configuration.nix
-    ];
+      ({ pkgs, ... }: {
+        imports = [
+          ./hardware-configuration.nix
+        ];
 
-    networking.hostName = "nixtop";
-    environment.systemPackages = [
-      self.packages.${pkgs.stdenv.hostPlatform.system}.davinci-resolve-studio
+        networking.hostName = "nixtop";
+        environment.systemPackages = [
+          self.packages.${pkgs.stdenv.hostPlatform.system}.davinci-resolve-studio
+        ];
+      })
     ];
   };
 }
