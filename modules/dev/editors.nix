4dc0a02168a5a8e1e59cf8b5543700eef54a6198
valblaze <valblazeyt@gmail.com>
2026-07-22 00:14:22 -0500

    sync

--- modules/dev/editors.nix
+++ modules/dev/editors.nix
@@ -8,9 +8,6 @@
   environment.systemPackages = with pkgs; [
     (inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.neovim)
     zed-editor
-    nil
-    nixd
-    lua-language-server
   ];
 
   environment.shellAliases = {
